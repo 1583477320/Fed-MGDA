4abd075501b9621bdc92f8cc9fd58242d5f002ce
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-07-16 17:43:42 +0800

    Update models.py

--- main/models.py
+++ main/models.py
@@ -27,30 +27,20 @@ class MLP(nn.Module):
 class CNNMnist(nn.Module):
     def __init__(self, args):
         super(CNNMnist, self).__init__()
-        self.shared_layer = nn.Sequential(
-            # 第一个卷积块
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(32),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.ReLU(),
-
-            # 第二个卷积块
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(64),
-            nn.Dropout(0.3),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.ReLU(),
-            
-            nn.Flatten(),  # 展平特征图
-            nn.Linear(3136, 128),
-            nn.ReLU(),
-            # nn.Dropout(),
-            nn.Linear(128, 10)
-        )
+        self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, args.num_classes)
 
     def forward(self, x):
-        out = self.shared_layer(x)
-        return F.log_softmax(out, dim=1)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
 class Adult(nn.Module):   # logistic regression for adult dataset
     def __init__(self, dim_in=99, dim_out=2):
